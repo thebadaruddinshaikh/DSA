304d9d6e61d19d6994dcfa397bdfb6f344ab73d0
thebadaruddinshaikh <the.badaruddinshaikh@gmail.com>
2022-01-22 22:52:25 +0530

    Updated .gitIgnore to Ignore Incomplete project files

--- sorting/Counting.java
+++
@@ -1,4 +0,0 @@
-package com.dsa.sorting;
-
-public class Counting {
-}
--- sorting/Insertion.java
+++ sorting/Insertion.java
@@ -1,4 +1,28 @@
-package com.dsa.sorting;
+// package com.dsa.sorting;
 
 public class Insertion {
+  public static void main(String[] args) {
+    int[] arr = { 6, 2, 8, 9, 3, 4, 0, 6, 1, 5, 7 };
+    Insertion insertion = new Insertion();
+
+    insertion.sort(arr);
+
+    for (int i : arr)
+      System.out.print(i);
+
+  }
+  
+  public void sort(int[] arr) {
+    
+    for (int j = 0; j < arr.length; j++) {
+      int key = arr[j];
+      int i = j - 1;
+
+      while (i > -1 && key < arr[i]) {
+        arr[i + 1] = arr[i];
+        i--;
+      }
+      arr[i + 1] = key;
+    }
+  }
 }
